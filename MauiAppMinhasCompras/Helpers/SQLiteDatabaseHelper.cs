fbb6fe4436d150b70aa1a56051a33f1b5113f2c7
JaquePaula <jaquemarques.santos@gmail.com>
2025-03-09 12:44:18 -0300

    Adição da Funcionalidade de Inserção no SQLite

--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -11,13 +11,14 @@ namespace MauiAppMinhasCompras.Helpers
 
    public class SQLiteDatabaseHelper
    {
-    readonly SQLiteAsyncConnection _conn;
-
+    readonly SQLiteAsyncConnection _conn; 
 
     public SQLiteDatabaseHelper(string path){
       _conn = new SQLiteAsyncConnection(path);
       _conn.CreateTableAsync<Produto>().Wait();
     }
+
+// CRUD
 
     public Task<int> Insert(Produto p)
     {
