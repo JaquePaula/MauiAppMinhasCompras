d8bb889f52fca15a1dfd56bf5911ef37d18ba1ca
JaquePaula <jaquemarques.santos@gmail.com>
2025-03-13 19:59:45 -0300

    Add: listagem, getAll, Soma, search

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -5,30 +5,30 @@ namespace MauiAppMinhasCompras;
 public partial class App : Application
 {
 
-	 static SQLiteDatabaseHelper _db;
+    static SQLiteDatabaseHelper _db;
 
-	 public static SQLiteDatabaseHelper Db 
-	 {
-		 get
+    public static SQLiteDatabaseHelper Db
+    {
+        get
+        {
+            if (_db == null)
             {
-                if(_db == null)
-                {
-                    string path = Path.Combine(
-                        Environment.GetFolderPath(
-                            Environment.SpecialFolder.LocalApplicationData),
-                        "banco_sqlite_compras.db3");
+                string path = Path.Combine(
+                    Environment.GetFolderPath(
+                        Environment.SpecialFolder.LocalApplicationData),
+                    "banco_sqlite_compras.db3");
 
-                    _db = new SQLiteDatabaseHelper(path);
-                }
-
-                return _db;
+                _db = new SQLiteDatabaseHelper(path);
             }
-	 }
-	
-	public App()
-	{
-		InitializeComponent();
-		// MainPage = new AppShell();
-		MainPage = new NavigationPage(new Views.ListaProduto());
-	}
+
+            return _db;
+        }
+    }
+
+    public App()
+    {
+        InitializeComponent();
+        // MainPage = new AppShell();
+        MainPage = new NavigationPage(new Views.ListaProduto());
+    }
 }
